d15af8d9ac55b15e24cf362e25b8a83fd034a49d
Razan Armouti <razanarmote@yahoo.com>
2021-09-18 21:22:41 +0300

    lab10

--- controllers/Weather.controller.js
+++ controllers/Weather.controller.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const WeatherController = async (req, res) => {
   let lat = req.query.lat;
   let lon = req.query.lon;
-  
+
 
   let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
 
@@ -19,6 +19,8 @@ const WeatherController = async (req, res) => {
     console.log(cleanedData);
     res.status(200).json(cleanedData);
 
+  }).catch(error => {
+    res.send(error);
   });
 
 
--- controllers/yelp.controller.js
+++ controllers/yelp.controller.js
@@ -6,9 +6,12 @@ let cache=new Cache();
 
 
 const YelpController = async (req, res) => {
-  let cityName = req.query.searchQuery;
+  let cityName = req.query.location;
+  let lat = req.query.lat;
+  let lon = req.query.lon;
   const cityNameYelp = `yelp-${cityName}`;
-  let url = `https://api.yelp.com/v3/businesses/search?api_key=${process.env.YELP_API_KEY}&searchQuery=${cityName}`;
+  let url = `https://api.yelp.com/v3/businesses/search?`;
+  // api_key=${process.env.YELP_API_KEY}&searchQuery=${cityName}
   // const yelpURL = ``;
   //   if(cache.data.length>0 && cache.date.cityName===cityName){
   //     res.json({'data':cache,'message':'data retrieved from the cache'});
@@ -16,21 +19,35 @@ const YelpController = async (req, res) => {
     res.send(cache[cityNameYelp]);
   }else{
 
-    axios.get(url).then((YelpData) => {
-      let cleanedData = YelpData.data.results.map(yelp =>{
+    axios.get(url,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+        },
+        params: {
+          term: 'restaurants',
+          location:cityName,
+          latitude:lat,
+          longitude:lon
+        },
+      },
+    ).then(YData=> {
+      let cleanedData =YData.data.businesses.map(y =>{
         // cache.data=res.data;
         // cache.cityName=cityName;
         // res.json({'data':cache.data,'message':'data is coming from the api'});
-        return new YelpModel(yelp.name,
-          yelp.image_url,
-          yelp.price,
-          yelp.rating,
-          yelp.url);
+        return new YelpModel(y.name,
+          y.image_url,
+          y.price,
+          y.rating,
+          y.url);
       });
       console.log(cleanedData);
       cache[cityNameYelp] = cleanedData;
       res.status(200).json(cleanedData);
 
+    }).catch(error => {
+      res.send(error);
     });
 
   }
